4bded35837d11be69c21c23381a54c95e4cee704
DeborahRios <43423330+DeborahRios@users.noreply.github.com>
2023-08-18 17:10:41 -0400

    Update streamlit_app.py
    remove some print values

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,10 @@ my_catalog = my_cur.fetchall()
 df = pandas.DataFrame(my_catalog)
 
 # temp write the dataframe to the page so I Can see what I am working with
-streamlit.write(df)
+#streamlit.write(df)
 # put the first column into a list
 color_list = df[0].values.tolist()
-streamlit.write(color_list)
+#streamlit.write(color_list)
 
 # print(color_list)
 # Let's put a pick list here so they can pick the color
@@ -31,7 +31,7 @@ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
-streamlit.write(df2)
+#streamlit.write(df2)
 streamlit.image(df2[0],width=400,caption= product_caption)
 
 streamlit.write('Price: ', df2[1])
